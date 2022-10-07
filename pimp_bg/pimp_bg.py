1069526f72e25dd97c7afd542b67723cd7ab6f52
mbecel <mael1.becel@epitech.eu>
2022-10-07 14:21:50 +0200

    ✨🐛🚧 (pimp_bg/pimp_bg.py): Add a better draw

--- pimp_bg/pimp_bg.py
+++ pimp_bg/pimp_bg.py
@@ -3,6 +3,8 @@ from PIL import Image, ImageDraw, ImageFont
 import datetime
 import subprocess
 
+window = [1920, 1080]
+
 def get_time(time : str):
     if (time < 10):
         return "0" + str(time)
@@ -14,18 +16,19 @@ now = datetime.datetime.now()
 
 file = random.choice(os.listdir("/home/mbecel/scripts/pimp_bg/images/"))
 os.system(f"cp ~/scripts/pimp_bg/images/{file} ~/scripts/pimp_bg/bg.png")
-
 background = Image.open("/home/mbecel/scripts/pimp_bg/bg.png")
-
 font_path = "/home/mbecel/scripts/pimp_bg/Timeless.ttf"
-font = ImageFont.truetype(font=font_path, size=70)
-out = subprocess.Popen(['./home/mbecel/scripts/pimp_bg/scripts/getplace'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+out = subprocess.Popen(['/bin/bash', '/home/mbecel/scripts/pimp_bg/scripts/getplace'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 stdout, stderr = out.communicate()
 print(stdout.decode('ASCII'))
 
-ImageDraw.Draw(background).text((1700, 40), f"{get_time(now.hour)}:{get_time(now.minute)}", (255, 255, 255), font=font)
-font = ImageFont.truetype(font=font_path, size=10)
-ImageDraw.Draw(background).text((1700, 120), f"{stdout.decode('ASCII')}", (255, 255, 255), font=font)
+
+font = ImageFont.truetype(font=font_path, size=110)
+ImageDraw.Draw(background).text((1450, 40), f"{get_time(now.hour)}:{get_time(now.minute)}", (255, 255, 255), font=font)
+
+font = ImageFont.truetype(font=font_path, size=18)
+ImageDraw.Draw(background).text((1750, 50), f"{stdout.decode('ASCII')}", (255, 255, 255), font=font)
 
 
 background.save("/home/mbecel/scripts/pimp_bg/bg.png")
