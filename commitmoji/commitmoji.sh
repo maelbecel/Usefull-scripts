fe455852e436718935cb453c077c9fbaa92047b4
mbecel <mael1.becel@epitech.eu>
2023-01-06 14:04:01 +0100

    🐛 (commitmoji/): I did it for the lulz!

--- commitmoji/commitmoji.sh
+++ commitmoji/commitmoji.sh
@@ -187,7 +187,7 @@ function main()
     fi
     if [[ "$1" == "-wm" || "$1" == "-m" ]]; then
         echo "Bangarang"
-        ffplay -nodisp -autoexit ~/scripts/commitmoji/Bangarang.mp3 >/dev/null
+        ffplay -nodisp -autoexit ~/scripts/commitmoji/Bangarang.mp3 >/dev/null 2>&1
     fi
 }
 
